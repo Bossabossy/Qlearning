2ed57209614052276105dc131af0839f5ef4b030
wasit7 <wasit7@gmail.com>
2017-08-01 15:57:47 +0700

    updated Env

--- Strawry/env.py
+++ Strawry/env.py
@@ -1,14 +1,26 @@
 class Env(object):
-"""
-    Strawry environment ...
-"""
+    """
+        Strawry environment ...
+    """
+    def cap(self,x, down, up, ninter):
+        if x<down:
+            x=down
+        if up<=x:
+            x=up-1
+        step=(up-down)/ninter
+        #print x
+        return (x-down)//step
+
     def __init__(self):
         self.n_action=16
-        self.n_state=500
+        self.n_state=500        
+        self.max_step=100
+        self.reset()
+
     def get_state(self):        
-        x1=cap(self.temp, 10, 25, 5)
-        x2=cap(self.humi, 10, 100, 5)
-        x3=cap(self.co2, 300, 3000, 5)
+        x1=self.cap(self.temp, 10, 25, 5)
+        x2=self.cap(self.humi, 10, 100, 5)
+        x3=self.cap(self.co2, 300, 3000, 5)
         x4=self.light
         x5=self.watp
         return x1*5*5*2*2 + x2*5*5*2*2 + x3*2*2 + x4*2 +x5
@@ -19,6 +31,8 @@ class Env(object):
         self.co2=co2
         self.light=light
         self.watp=watp
+        self.done=0
+        self.n_step=0
         return self.get_state()
     
     def set_target(self, temp=25, humi=80, co2=200, light=0, watp=0):
@@ -32,7 +46,7 @@ class Env(object):
     def get_reward(self):
         if self.t_temp-1<self.temp and self.temp<=self.t_temp+1:
             if self.t_humi-5<self.humi and self.humi<=self.t_humi+5:
-                return 1
+                return 1         
         return -0.1
     
     def code2int(self,code):
@@ -44,6 +58,7 @@ class Env(object):
         return int(b[-4]),int(b[-3]),int(b[-2]),int(b[-1])
     
     def step(self,a):
+        self.n_step+=1
         comp,mist,light,watp = self.int2code(a)
         if comp==1:
             self.humi-=5
@@ -55,8 +70,10 @@ class Env(object):
             self.temp+=1
         if watp==1:
             self.humi+=1
-        return self.get_state(), self.get_reward(), 0, 0
+        if self.max_step <= self.n_step:
+            self.done=1
+        return self.get_state(), self.get_reward(), self.done, 0
     
     def render(self):
-        print( "temp:{}, humi:{}, co2:{}, light:{}, watp{}".format(
-             self.temp, self.humi, self.co2, self.light, self.watp))
+        print( "step:{} temp:{}, humi:{}, co2:{}, light:{}, watp{}".format(
+             self.n_step, self.temp, self.humi, self.co2, self.light, self.watp))
